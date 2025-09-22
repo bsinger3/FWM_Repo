aee8cf03f5f8ccec4e51fa4459a3572d6bcf7f9c
Bri Singer <you@example.com>
2025-09-22 18:01:45 -0400

    chore(format): Prettier + config tidy

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 window.SUPABASE_URL = 'https://kmomndloorvrjzmiexxl.supabase.co';
-window.SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imttb21uZGxvb3J2cmp6bWlleHhsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTczNTQyMzEsImV4cCI6MjA3MjkzMDIzMX0.pjw2qxtN49-oC9e7i59PYwh0eMXDYuVVDx-9RkRlzn0';
+window.SUPABASE_ANON_KEY =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imttb21uZGxvb3J2cmp6bWlleHhsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTczNTQyMzEsImV4cCI6MjA3MjkzMDIzMX0.pjw2qxtN49-oC9e7i59PYwh0eMXDYuVVDx-9RkRlzn0';
 window.IMAGE_PROXY = 'https://fwm-proxy.bsinger3.workers.dev/?url=';
